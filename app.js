686458d22283e5e79969c644072c413582812f8a
Abdul Samad <abdulsamadborn03@gmail.com>
2021-12-18 19:26:57 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+
+
+
+function password(){
+
+    var password = document.getElementById('input1').value;
+
+    if (password === 'samad'){
+        document.write('correct password');
+    }
+    else{
+        document.write('wrong password');
+    }
+}
+
+
